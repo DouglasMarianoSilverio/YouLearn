bdd1fcbc9fd8dc9e21f089b2a3e909b5764d2c78
Douglas Mariano Silverio <douglas07@gmail.com>
2018-07-17 22:58:44 -0300

    Commit inicial

---
+++ YouLearn.Domain/Entities/Favorito.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YouLearn.Domain.Entities
+{
+    public class Favorito
+    {
+        public Guid Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Video Video { get; set; }
+
+    }
+}
---
+++ YouLearn.Domain/Entities/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YouLearn.Domain.Entities
+{
+    public class Video
+    {
+        public Guid Id { get; set; }
+        public Canal Canal { get; set; }
+        public Playlist Playlist { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Tags { get; set; }
+        public int OrdemNaPlaylist { get; set; }
+        public string IdVideoYoutube { get; set; }
+        public Usuario UsuarioSugeriu { get; set; }
+        public string Status { get; set; }
+    }
+}
