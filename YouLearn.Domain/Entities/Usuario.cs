054d8ebb26b31c5342ba2f138c092f7d1a7c6f26
Douglas Mariano Silverio <douglas07@gmail.com>
2018-11-25 18:40:36 -0200

    Seção 2 Aula 13

--- YouLearn.Domain/Entities/Usuario.cs
+++ YouLearn.Domain/Entities/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using prmToolkit.NotificationPattern;
 using prmToolkit.NotificationPattern.Extensions;
+using YouLearn.Domain.Extensions;
 using System;
 using YouLearn.Domain.Entities.Base;
 using YouLearn.Domain.Resources;
@@ -9,21 +10,38 @@ namespace YouLearn.Domain.Entities
 {
     public class Usuario : EntityBase
     {
+        public Usuario(Email email, string senha)
+        {
+            Email = email;
+            Senha = senha;
+
+            AddNotifications(Email);
+
+            new AddNotifications<Usuario>(this)
+             .IfLengthLowerThan(x => x.Senha, 3, MSG.X0_E_OBRIGATORIA_E_DEVE_CONTER_X1_CARACTERES.ToFormat("Senha", 4));
+
+            Senha = Senha.ConvertToMD5();
+        }
+
         public Usuario(Nome nome, Email email, string senha)
         {
             Nome = nome;
             Email = email;
             Senha = senha;
 
+            AddNotifications(Nome, Email);
+
             new AddNotifications<Usuario>(this)
                 .IfLengthLowerThan(x => x.Senha, 3, MSG.X0_E_OBRIGATORIA_E_DEVE_CONTER_X1_CARACTERES.ToFormat("Senha", 4));
+
+            Senha = Senha.ConvertToMD5();
         }
 
-        public Nome Nome { get; set; }
-        public Email Email { get; set; }
-        public string  Senha { get; set; }
+        public Nome Nome { get; private set; }
+        public Email Email { get; private set; }
+        public string Senha { get; private set; }
 
-        
+
 
     }
 }
--- YouLearn.Domain/Services/ServiceUsuario.cs
+++ YouLearn.Domain/Services/ServiceUsuario.cs
@@ -1,21 +1,26 @@
 ﻿using prmToolkit.NotificationPattern;
-<<<<<<< HEAD
 using prmToolkit.NotificationPattern.Extensions;
-=======
->>>>>>> 491f5a4b0e68118431d4b3f7a29475c327fba264
 using System;
 using YouLearn.Domain.Arguments.Usuario;
 using YouLearn.Domain.Entities;
+using YouLearn.Domain.Interfaces.Repositories;
 using YouLearn.Domain.Interfaces.Services;
 using YouLearn.Domain.Resources;
 using YouLearn.Domain.ValueObjects;
 
 namespace YouLearn.Domain.Services
 {
- 
+
     public class ServiceUsuario : Notifiable, IServiceUsuario
- 
+
     {
+        private readonly IRepositoryUsuario _repositoryUsuario;
+
+        public ServiceUsuario(IRepositoryUsuario repositoryUsuario)
+        {
+            _repositoryUsuario = repositoryUsuario;
+        }
+
         public AdicionarUsuarioResponse AdicionarUsuario(AdicionarUsuarioRequest request)
         {
             if (request == null)
@@ -25,31 +30,50 @@ namespace YouLearn.Domain.Services
                 return null;
             }
 
-            Nome nome = new Nome( request.PrimeiroNome,request.UltimoNome);
-        
-
+            Nome nome = new Nome(request.PrimeiroNome, request.UltimoNome);
             Email email = new Email(request.Email);
+            Usuario usuario = new Usuario(nome, email, request.Senha);
 
-
-
-
-            Usuario usuario = new Usuario(
-           nome, email, request.Senha);
-            
-
-            AddNotifications(nome, email,usuario);
-
-
-      
+            AddNotifications( usuario);
+            if (this.IsInvalid())
+            {
+                return null; 
+            }
             //persiste
-            return new AdicionarUsuarioResponse(Guid.NewGuid());
+            _repositoryUsuario.Salvar(usuario);
+            return new AdicionarUsuarioResponse(usuario.Id);
 
 
         }
 
         public AutenticarUsuarioResponse AutenticarUsuario(AutenticarUsuarioRequest request)
         {
-            throw new System.NotImplementedException();
+            if (request == null)
+            {
+                AddNotification("AutenticarUsuarioRequest", MSG.OBJETO_X0_E_OBRIGATORIO.ToFormat("AutenticarUsuarioRequest"));
+                return null;
+            }
+
+            var email = new Email(request.Email);
+            var usuario = new Usuario(email, request.Senha);
+
+            AddNotifications(usuario);
+
+            if (this.IsInvalid()) return null;
+            usuario = _repositoryUsuario.Obter(usuario.Email.Endereco, usuario.Senha);
+
+            if (usuario == null)
+            {
+                AddNotification("Usuario", MSG.DADOS_NAO_ENCONTRADOS);
+            }
+
+            var response = new AutenticarUsuarioResponse()
+            {
+                Id = usuario.Id,
+                PrimeiroNome = usuario.Nome.PrimeiroNome
+            };
+
+            return response;
         }
     }
 }
