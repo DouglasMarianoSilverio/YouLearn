040df75adf9a02141de8c590fdb422bf3f831084
Douglas Mariano Silverio <douglas07@gmail.com>
2020-03-27 18:47:20 -0300

    Video controller added

--- YouLearn.Api/Controllers/CanalController.cs
+++ YouLearn.Api/Controllers/CanalController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using YouLearn.Domain.Arguments.Canal;
 using YouLearn.Domain.Interfaces.Services;
 using YouLearn.Infra.Transactions;
 
@@ -27,6 +28,39 @@ namespace YouLearn.Api.Controllers
                 var response = _serviceCanal.Listar(idusuario);
                 return await ResponseAsync(response, _serviceCanal);
 
+            }
+            catch (Exception ex)
+            {
+
+                return await ResponseExceptionAsync(ex);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/v1/Canal/Adicionar")]
+        public async Task<IActionResult> Adicionar([FromBody] AdicionarCanalRequest request )
+        {
+            try
+            {
+                Guid idusuario = Guid.NewGuid();
+                var response = _serviceCanal.AdicionarCanal(request, idusuario);
+                return await ResponseAsync(response, _serviceCanal);
+            }
+            catch (Exception ex)
+            {
+
+                return await ResponseExceptionAsync(ex);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/v1/Canal/Excluir")]
+        public async Task<IActionResult> Excluir(Guid idCanal)
+        {
+            try
+            {                
+                var response = _serviceCanal.ExcluirCanal(id);
+                return await ResponseAsync(response, _serviceCanal);
             }
             catch (Exception ex)
             {
